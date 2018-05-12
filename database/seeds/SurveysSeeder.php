fe45196df426bdb2ae815770da97f6a3205493cf
Marcelo Espinoza <mespinozazuarez@gmail.com>
2018-05-11 22:35:25 -0300

    Update seeder

--- database/seeds/SurveysSeeder.php
+++ database/seeds/SurveysSeeder.php
@@ -11,15 +11,39 @@ class SurveysSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Survey::class, 1)->create();
-        $question_types = factory(App\QuestionType::class, 5)->create();
+        //$question_types = factory(App\QuestionType::class, 5)->create();
+        $question_type = new App\QuestionType();
+        $question_type->type = "Selección múltiple";
+        $question_type->description = "Es posible seleccionar varias respuestas de las opciones entregadas";
+        $question_type->save();
+        $question_type = new App\QuestionType();
+        $question_type->type = "Selección única";
+        $question_type->description = "Es posible seleccionar solo una de las opciones entregadas";
+        $question_type->save();
+        $question_types = App\QuestionType::all();
         $question_types->each(function($question_type){
             $questions = factory(App\Question::class, 10)->make();
             $question_type->questions()->saveMany($questions);
             $questions->each(function($question){
-                    $response_choises = factory(App\ResponseChoise::class, 5)->make();
-                    $question->response_choises()->saveMany($response_choises);
-                    });
+                $response_choises = factory(App\ResponseChoise::class, 5)->make();
+                $question->response_choises()->saveMany($response_choises);
+                });
         });
+        factory(App\Survey::class, 1)->create();
+        $surveyQuestion = new App\SurveyQuestion();
+        $surveyQuestion->survey_id = 1;
+        $surveyQuestion->question_id = 1;
+        $surveyQuestion->order = 3;
+        $surveyQuestion->save();
+        $surveyQuestion = new App\SurveyQuestion();
+        $surveyQuestion->survey_id = 1;
+        $surveyQuestion->question_id = 15;
+        $surveyQuestion->order = 2;
+        $surveyQuestion->save();
+        $surveyQuestion = new App\SurveyQuestion();
+        $surveyQuestion->survey_id = 1;
+        $surveyQuestion->question_id = 20;
+        $surveyQuestion->order = 1;
+        $surveyQuestion->save();
     }
 }
